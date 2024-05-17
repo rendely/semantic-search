a180d03c58f7fd7ef67641f3d031c77d705cbba8
matthew rendely <matthew.rendely@gmail.com>
2024-05-16 21:56:21 -0700

    Add fake profile and topk for answers

--- script.js
+++ script.js
@@ -1,9 +1,72 @@
 import { pipeline } from '@xenova/transformers';
 
 const answerer = await pipeline('question-answering', 'Xenova/distilbert-base-uncased-distilled-squad');
-const question = 'Insurance number?';
-const context = 'Health insurance: 123456\nFirst name: John\nLast name: Snow';
-const output = await answerer(question, context);
+
+const question = 'What is the KTN number?';
+const context = `
+First Name: John
+Last Name: Snow
+Date of Birth: 1990-05-15
+Gender: Male
+Marital Status: Single
+Phone Number: +1 (555) 123-4567
+Email: john.snow@example.com
+Address: 123 Winterfell Lane, Castle Black, The North, Westeros, 01010
+Social Security Number (SSN): 123-45-6789
+
+
+Health Insurance: 123456
+Primary Care Physician: Dr. Maester Aemon
+Allergies: Penicillin
+Current Medications: None
+Blood Type: O+
+Emergency Contact: Arya Stark (+1 (555) 987-6543)
+
+
+Current Employer: Night's Watch
+Job Title: Lord Commander
+Department: Defence
+Work Email: john.snow@thenightswatch.org
+Work Phone Number: +1 (555) 890-1234
+Office Address: The Wall, Castle Black, The North, Westeros, 01010
+
+
+Highest Degree Obtained: Bachelor of Arms
+University: The Citadel
+Graduation Year: 2012
+Major: Battle Tactics and Leadership
+
+
+Passport Number: W12345678
+Nationality: Westerosi
+Frequent Flyer Number: Royal Air: 56789-123
+Known Traveler Number (KTN): KTN1234567890
+Preferred Airlines: Royal Air, Dragonfly Air
+Destination Preferences: Dragonstone, King's Landing, Winterfell
+
+
+Bank Name: Iron Bank of Braavos
+Account Number: 000123456789
+Routing Number: 110000000
+Credit Card Type: Visa
+Credit Card Number: 4111 1111 1111 1111
+Expiration Date: 01/25
+CVV: 123
+
+
+LinkedIn: [linkedin.com/in/johnsnow](https://linkedin.com/in/johnsnow)
+Twitter: [@johnsnow_nw](https://twitter.com/johnsnow_nw)
+Facebook: [facebook.com/john.snow.nightswatch](https://facebook.com/john.snow.nightswatch)
+Instagram: [@lordcommander_snow](https://instagram.com/lordcommander_snow)
+
+
+Favorite Food: Roast Direwolf
+Hobbies: Swordfighting, Horse Riding, Exploring the North
+Pet Name: Ghost
+Pet Type: Direwolf
+Languages Spoken: Common Tongue, Dothraki, Valyrian`
+
+const output = await answerer(question, context, {topk:2} );
 
 console.log(output)
 
@@ -11,8 +74,6 @@ const extractor = await pipeline('feature-extraction', 'Xenova/all-MiniLM-L6-v2'
 const output2 = await extractor('This is a simple test.', { pooling: 'mean', normalize: true });
 
 const output3 = await extractor('This is a complicated problem.', { pooling: 'mean', normalize: true });
-
-console.log(output2.data)
 
 function cosineSimilarity(arr1, arr2) {
 if (arr1.length !== arr2.length) {
@@ -38,9 +99,5 @@ throw new Error('One of the arrays is zero-length.');
 
 return dotProduct / (magnitudeA * magnitudeB);
 }
-
-// Example usage:
-const arr1 = new Float32Array([1, 2, 3]);
-const arr2 = new Float32Array([4, 5, 6]);
 
 console.log(cosineSimilarity(output2.data, output3.data)); // Output: 0.9746318461970762
