6cae1e98392d04bff441097fac276d3414334e3e
matthew rendely <matthew.rendely@gmail.com>
2024-05-18 14:39:57 -0700

    Add cosine similarity search, better results

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import { pipeline } from '@xenova/transformers';
 
 const answerer = await pipeline('question-answering', 'Xenova/distilbert-base-uncased-distilled-squad');
 
-const question = 'What is the KTN?';
+const question = 'You are on growlerz.com Custom Portal page. What should you input to the form field asking about Pet Name?';
 const context = `
 First Name: John.
 Last Name: Snow.
@@ -63,10 +63,84 @@ const output = await answerer(question, context, {topk:10} );
 
 console.log(output)
 
-const extractor = await pipeline('feature-extraction', 'Xenova/all-MiniLM-L6-v2');
-const output2 = await extractor('This is a simple test.', { pooling: 'mean', normalize: true });
 
-const output3 = await extractor('This is a complicated problem.', { pooling: 'mean', normalize: true });
+const data = [
+    {"key": "First Name", "value": "John"},
+    {"key": "Last Name", "value": "Snow"},
+    {"key": "Date of Birth", "value": "1990-05-15"},
+    {"key": "Gender", "value": "Male"},
+    {"key": "Marital Status", "value": "Single"},
+    {"key": "Phone Number", "value": "+1 (555) 123-4567"},
+    {"key": "Email", "value": "john.snow@example.com"},
+    {"key": "Address", "value": "123 Winterfell Lane, Castle Black, The North, Westeros, 01010"},
+    {"key": "Social Security Number (SSN)", "value": "123-45-6789"},
+    {"key": "Health Insurance", "value": "123456"},
+    {"key": "Primary Care Physician", "value": "Dr. Maester Aemon"},
+    {"key": "Allergies", "value": "Penicillin"},
+    {"key": "Current Medication", "value": "None"},
+    {"key": "Blood Type", "value": "O+"},
+    {"key": "Emergency Contact", "value": "Arya Stark (+1 (555) 987-6543)"},
+    {"key": "Current Employer", "value": "Night's Watch"},
+    {"key": "Job Title", "value": "Lord Commander"},
+    {"key": "Department", "value": "Defence"},
+    {"key": "Work Email", "value": "john.snow@thenightswatch.org"},
+    {"key": "Work Phone Number", "value": "+1 (555) 890-1234"},
+    {"key": "Office Address", "value": "The Wall, Castle Black, The North, Westeros, 01010"},
+    {"key": "Highest Degree Obtained", "value": "Bachelor of Arms"},
+    {"key": "University", "value": "The Citadel"},
+    {"key": "Graduation Year", "value": "2012"},
+    {"key": "Major", "value": "Battle Tactics and Leadership"},
+    {"key": "Passport Number", "value": "W12345678"},
+    {"key": "Nationality", "value": "Westerosi"},
+    {"key": "Frequent Flyer Number", "value": "Royal Air: 56789-123"},
+    {"key": "Known Traveler Number (KTN)", "value": "KTN1234567890"},
+    {"key": "Preferred Airlines", "value": "Royal Air, Dragonfly Air"},
+    {"key": "Destination Preferences", "value": "Dragonstone, King's Landing, Winterfell"},
+    {"key": "Bank Name", "value": "Iron Bank of Braavos"},
+    {"key": "Account Number", "value": "000123456789"},
+    {"key": "Routing Number", "value": "110000000"},
+    {"key": "Credit Card Type", "value": "Visa"},
+    {"key": "Credit Card Number", "value": "4111 1111 1111 1111"},
+    {"key": "Expiration Date", "value": "01/25"},
+    {"key": "CVV", "value": "123"},
+    {"key": "LinkedIn", "value": "[linkedin.com/in/johnsnow](https://linkedin.com/in/johnsnow)"},
+    {"key": "Twitter", "value": "[@johnsnow_nw](https://twitter.com/johnsnow_nw)"},
+    {"key": "Facebook", "value": "[facebook.com/john.snow.nightswatch](https://facebook.com/john.snow.nightswatch)"},
+    {"key": "Instagram", "value": "[@lordcommander_snow](https://instagram.com/lordcommander_snow)"},
+    {"key": "Favorite Food", "value": "Roast Direwolf"},
+    {"key": "Hobbies", "value": "Swordfighting, Horse Riding, Exploring the North"},
+    {"key": "Pet Name", "value": "Ghost"},
+    {"key": "Pet Type", "value": "Direwolf"},
+    {"key": "Languages Spoken", "value": "Common Tongue, Dothraki, Valyrian"}
+    ]
+
+
+
+const extractor = await pipeline('feature-extraction', 'Xenova/all-MiniLM-L6-v2');
+
+await Promise.all(data.map(async (d) => {
+    d.embed = await extractor(d.key,  { pooling: 'mean', normalize: true });
+    return d
+}));
+
+// console.log(data.slice(0,4))
+const query = {key: 'TSA Pre'}
+query.embed = await extractor(query.key,  { pooling: 'mean', normalize: true });
+
+const results = await Promise.all(
+    data.map(async (d) => {
+        d.score = cosineSimilarity(query.embed.data, d.embed.data);
+        return d
+    })
+);
+
+results.sort((a,b) => b.score - a.score)
+console.log(results.slice(0,3));
+
+// console.log(cosineSimilarity(query.embed.data, data[9].embed.data)); // Output: 0.
+// const output2 = await extractor('This is a simple test.', { pooling: 'mean', normalize: true });
+
+// const output3 = await extractor('This is a complicated problem.', { pooling: 'mean', normalize: true });
 
 function cosineSimilarity(arr1, arr2) {
 if (arr1.length !== arr2.length) {
@@ -93,4 +167,6 @@ throw new Error('One of the arrays is zero-length.');
 return dotProduct / (magnitudeA * magnitudeB);
 }
 
-console.log(cosineSimilarity(output2.data, output3.data)); // Output: 0.9746318461970762
+// console.log(cosineSimilarity(output2.data, output3.data)); 
+
+// Output: 0.9746318461970762
