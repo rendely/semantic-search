84e13f1eb33262d4aa27b8bfc31bfae5454f3e73
matthew rendely <matthew.rendely@gmail.com>
2024-05-19 22:38:57 -0700

    Add I'm feeling lucky shortcut to fill all

--- src/content.js
+++ src/content.js
@@ -2,15 +2,16 @@
 // to the DOM and other web APIs.
 
 // Example usage:
-function classify(query, target) {
-
+function classify(query, target, isLucky = false) {
     const message = {
         action: 'classify',
         text: query,
     }
     chrome.runtime.sendMessage(message, (response) => {
-        console.log('received user data', response);
-        target.value = response[ansIndex].value;
+        // console.log('received user data', response);
+        const answer =  response[(isLucky ? 0 : ansIndex)].value;
+        console.log(query, answer, ansIndex);
+        target.value = answer;
         target.style.backgroundColor = 'cyan';
         ansIndex++;
         if (ansIndex > 4) ansIndex = 0;
@@ -20,7 +21,12 @@ function classify(query, target) {
 
 document.addEventListener('keydown', (e) => {
     if (e.key === 'i' && e.metaKey){
-        handleFocus({target: document.activeElement})
+        autofillElement({target: document.activeElement})
+    }
+    if (e.key === 'u' && e.metaKey){
+        const inputs = document.querySelectorAll('input');
+        const textInputs = Array.from(inputs).filter(i => i.type === 'text');
+        textInputs.forEach(t => autofillElement({target: t}))
     }
 },true);
 
@@ -28,13 +34,17 @@ let ansIndex = 0;
 
 document.addEventListener('focus', (e) => ansIndex = 0, true);
 
-// document.addEventListener('focus', handleFocus, true);
+document.addEventListener('blur', saveInputs, true);
 
-function handleFocus(e) {
+function autofillElement(e) {
     const parentString = e.target.parentElement.innerText;
     const grandparentString = e.target.parentElement.parentElement.innerText;
     const placeholder = e.target.placeholder;
     const query = (parentString.length < 4 ? grandparentString : parentString) + placeholder;
-    console.log(query);
-    classify(query, e.target);
+    classify(query, e.target, true);
+}
+
+function saveInputs(e){
+    if (e.nodeName !== 'input' && e.type !== 'text') return 
+    console.log(e)
 }
