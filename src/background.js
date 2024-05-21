a77e6b694ac633e01a8e69d8de5778eb0120a20c
matthew rendely <matthew.rendely@gmail.com>
2024-05-20 20:39:08 -0700

    Refactor cleanup

--- src/background.js
+++ src/background.js
@@ -1,67 +1,5 @@
-// background.js - Handles requests from the UI, runs the model, then sends back a response
-
 import { pipeline, env } from '@xenova/transformers';
-
-
-const data = [
-    { "key": "First Name", "value": "John" },
-    { "key": "Last Name", "value": "Snow" },
-    { "key": "Date of Birth", "value": "1990-05-15" },
-    { "key": "Gender", "value": "Male" },
-    { "key": "Marital Status", "value": "Single" },
-    { "key": "Phone", "value": "+1 (555) 123-4567" },
-    { "key": "Phone number", "value": "+1 (555) 123-4567" },
-    { "key": "Mobile", "value": "+1 (555) 123-4567" },
-    { "key": "Email", "value": "john.snow@example.com" },
-    { "key": "Why do you want to work here", "value": "Excited about opportunity" },
-    { "key": "Address", "value": "123 Winterfell Lane, Castle Black, The North, Westeros, 01010" },
-    { "key": "City", "value": "Castle Black" },
-    { "key": "Country", "value": "The North" },
-    { "key": "Social Security Number (SSN)", "value": "123-45-6789" },
-    { "key": "Health Insurance", "value": "123456" },
-    { "key": "Primary Care Physician", "value": "Dr. Maester Aemon" },
-    { "key": "Allergies", "value": "Penicillin" },
-    { "key": "Current Medication", "value": "None" },
-    { "key": "Blood Type", "value": "O+" },
-    { "key": "Emergency Contact", "value": "Arya Stark (+1 (555) 987-6543)" },
-    { "key": "Current Employer", "value": "Night's Watch" },
-    { "key": "Job Title", "value": "Lord Commander" },
-    { "key": "Department", "value": "Defence" },
-    { "key": "Compensation", "value": "$100k" },
-    { "key": "Salary", "value": "$100k" },
-    { "key": "Desired salary", "value": "$100k" },
-    { "key": "Work Email", "value": "john.snow@thenightswatch.org" },
-    { "key": "Work Phone Number", "value": "+1 (555) 890-1234" },
-    { "key": "Office Address", "value": "The Wall, Castle Black, The North, Westeros, 01010" },
-    { "key": "Highest Degree Obtained", "value": "Bachelor of Arms" },
-    { "key": "Earliest start date, begin date", "value": "July 2024" },
-    { "key": "Times available to work", "value": "Mon-Fri 9am to 5pm" },
-    { "key": "University", "value": "The Citadel" },
-    { "key": "Graduation Year", "value": "2012" },
-    { "key": "Major", "value": "Battle Tactics and Leadership" },
-    { "key": "Passport Number", "value": "W12345678" },
-    { "key": "Nationality", "value": "Westerosi" },
-    { "key": "Frequent Flyer Number", "value": "56789-123" },
-    { "key": "Known Traveler Number (KTN)", "value": "KTN1234567890" },
-    { "key": "Preferred Airlines", "value": "Royal Air, Dragonfly Air" },
-    { "key": "Destination Preferences", "value": "Dragonstone, King's Landing, Winterfell" },
-    { "key": "Bank Name", "value": "Iron Bank of Braavos" },
-    { "key": "Account Number", "value": "000123456789" },
-    { "key": "Routing Number", "value": "110000000" },
-    { "key": "Credit Card Type", "value": "Visa" },
-    { "key": "Credit Card Number", "value": "4111 1111 1111 1111" },
-    { "key": "Expiration Date", "value": "01/25" },
-    { "key": "CVV", "value": "123" },
-    { "key": "LinkedIn", "value": "https://linkedin.com/in/johnsnow" },
-    { "key": "Twitter", "value": "https://twitter.com/johnsnow_nw" },
-    { "key": "Facebook", "value": "https://facebook.com/john.snow.nightswatch" },
-    { "key": "Instagram", "value": "https://instagram.com/lordcommander_snow" },
-    { "key": "Favorite Food", "value": "Roast Direwolf" },
-    { "key": "Hobbies", "value": "Swordfighting, Horse Riding, Exploring the North" },
-    { "key": "Pet Name", "value": "Ghost" },
-    { "key": "Pet Type", "value": "Direwolf" },
-    { "key": "Languages Spoken", "value": "Common Tongue, Dothraki, Valyrian" }
-]
+import {data} from './data.js';
 
 // Skip initial check for local models, since we are not loading any local models.
 env.allowLocalModels = false;
@@ -69,13 +7,6 @@ env.allowLocalModels = false;
 // Due to a bug in onnxruntime-web, we must disable multithreading for now.
 // See https://github.com/microsoft/onnxruntime/issues/14445 for more information.
 env.backends.onnx.wasm.numThreads = 1;
-
-
-// const answerer = await pipeline('question-answering', 'Xenova/distilbert-base-uncased-distilled-squad');
-
-// const output = await answerer('name', 'my name is matt', {topk:10} );
-
-// console.log(output);
 
 class PipelineSingleton {
     static task = 'feature-extraction';
@@ -105,66 +36,17 @@ const classify = async (text) => {
     return result;
 };
 
-classify('namess').then(r => console.log(r))
-
 data.map(async (d) => {
     d.embed = await classify(d.key);
     return d
 });
 
-console.log(data);
 
-// async function getData(){
-//   const data1 = await classify('name');
-//   const data2 = await classify('name2');
-//   console.log(data1);
-//   console.log(data2);
-//   console.log(cosineSimilarity(data1.data, data2.data));
-// }
-
-// getData();
-
-////////////////////// 1. Context Menus //////////////////////
-//
-// Add a listener to create the initial context menu items,
-// context menu items only need to be created at runtime.onInstalled
-chrome.runtime.onInstalled.addListener(function () {
-    // Register a context menu item that will only show up for selection text.
-    chrome.contextMenus.create({
-        id: 'classify-selection',
-        title: 'Classify "%s"',
-        contexts: ['selection'],
-    });
-});
-
-// Perform inference when the user clicks a context menu
-chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-    // Ignore context menu clicks that are not for classifications (or when there is no input)
-    if (info.menuItemId !== 'classify-selection' || !info.selectionText) return;
-
-    // Perform classification on the selected text
-    let result = await classify(info.selectionText);
-
-    // Do something with the result
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },    // Run in the tab that the user clicked in
-        args: [result],               // The arguments to pass to the function
-        function: (result) => {       // The function to run
-            // NOTE: This function is run in the context of the web page, meaning that `document` is available.
-            console.log('result', result)
-            console.log('document', document)
-        },
-    });
-});
-//////////////////////////////////////////////////////////////
-
-////////////////////// 2. Message Events /////////////////////
+////////////////////// Message Events /////////////////////
 // 
 // Listen for messages from the UI, process it, and send the result back.
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log('sender', sender)
 
-    //TODO: REFACTOR
     if (message.action === 'save') {
         console.log('Saving',message);
         const newData = message.save;
@@ -190,9 +72,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
             // Perform classification
 
             let result = await classify(message.text);
-            // sendResponse({data: data, result: result})
-            // console.log(result);
-            // console.log(data[0]);
+
             const results = await Promise.all(
                 data.map(async (d) => {
                     d.score = cosineSimilarity(result.data, d.embed.data);
@@ -200,10 +80,9 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
                 })
             );
             results.sort((a, b) => b.score - a.score)
-            // console.log(results.slice(0,3));
-            // Send response back to UI
+
             sendResponse(results.slice(0, 5));
-            // sendResponse(data[0].key)
+
         })();
 
         // return true to indicate we will send a response asynchronously
@@ -212,7 +91,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     }
 });
 //////////////////////////////////////////////////////////////
-
 
 
 function cosineSimilarity(arr1, arr2) {
